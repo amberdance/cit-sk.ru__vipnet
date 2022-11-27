742cfbc673cdef22058d369c081f4bd16c65cdde
alexeevch <106554488+alexeevch@users.noreply.github.com>
2022-11-27 22:07:57 +0300

    [feature] реализовал миграцию для User

---
+++ api/database/migrations/2022_11_27_180447_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->timestamps();
+            $table->string('login');
+            $table->string('password');
+            $table->string('name');
+            $table->string('patronymic')->nullable();
+            $table->string('surname')->nullable();
+            $table->string('email')->nullable();
+            $table->boolean('is_blocked')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
